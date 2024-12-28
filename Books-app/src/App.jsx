2f2fd9e7e51d9cde78a568eb38facbc50bbbb620
DLew <96327749+DLew2024@users.noreply.github.com>
2024-12-28 17:31:00 -0600

    Updated app with edit and delete functionality

--- Books-app/src/App.jsx
+++ Books-app/src/App.jsx
@@ -5,6 +5,23 @@ import BookList from "./components/BookList";
 function App() {
   const [books, setBooks] = useState([]);
 
+  const editBookByID = (id, newTitle) => {
+    const updatedBooks = books.map((book) => {
+      if (book.id === id) {
+        return { ...book, title: newTitle };
+      }
+    });
+    setBooks(updatedBooks);
+  };
+
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+
+    setBooks(updatedBooks);
+  };
+
   const createBook = (title) => {
     const updatedBooks = [
       ...books,
@@ -16,19 +33,10 @@ function App() {
     setBooks(updatedBooks);
   };
 
-  const editBook = () => {};
-
-  const deleteBookById = (id) => {
-    const updatedBooks = books.filter((book) => {
-      return book.id !== id;
-    });
-
-    setBooks(updatedBooks);
-  };
-
   return (
     <div className="app">
-      <BookList books={books} onDelete={deleteBookById} />
+      <h1>Reading List</h1>
+      <BookList books={books} onDelete={deleteBookById} onEdit={editBookByID} />
       <BookCreate onCreate={createBook} />
     </div>
   );
--- Books-app/src/components/BookEdit.jsx
+++ Books-app/src/components/BookEdit.jsx
@@ -1,5 +1,25 @@
-function BookEdit() {
-  return <div>BookEdit</div>;
+import { useState } from "react";
+
+function BookEdit({ book, onSubmit }) {
+  const [title, setTitle] = useState(book.title);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    onSubmit(book.id, title);
+  };
+
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="book-edit">
+      <label>Title</label>
+      <input onChange={handleChange} value={title} className="input" />
+      <button className="button is-primary">Save</button>
+    </form>
+  );
 }
 
 export default BookEdit;
